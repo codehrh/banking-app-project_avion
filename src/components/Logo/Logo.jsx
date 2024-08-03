facebf58a6c70d14fa8ca5e5cadb297348b1b477
mysunshinesunoo <caracajes95@gmail.com>
2024-08-03 09:55:01 +0800

    update aug 03 2024: span font size changes to css

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,10 +2,10 @@ import "./Logo.css";
 import '@fontsource/exo-2';
 import bankLogo from "../../assets/images/dinerobankicon.png";
 
-export default function Logo (){
-    return(
+export default function Logo() {
+    return (
         <div className="Logo">
-              <h1>Dinero <span><img src={bankLogo} alt="logo"></img></span> Bank</h1>  
+            <h1>Dinero <span><img src={bankLogo} alt="logo"></img></span> Bank</h1>
         </div>
     )
 }

d1da884896a94d39b81937270e912233d66a3b2b
Hara Honasan <honasan0@gmail.com>
2024-08-05 19:52:48 +0800

    calculator lesson for bankapp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Logo from "./components/Logo/Logo.jsx";
 import bankUsers from "./assets/data/bankUsers.json";
 import Menu from "./components/Menu/Menu.jsx";
 import BudgetApp from "./components/BudgetApp/BudgetApp.jsx";
+import { useState } from 'react';
 
 export default function App() {
   const bankEmployee = {
@@ -19,6 +20,55 @@ export default function App() {
     street: "Manila Street",
     country: "Philippines"
   }
+
+  //useState
+
+  // array destructuring
+  // [value/variable, function to update the value/variable]
+  // useState can accept different data types (string, number, or array)
+  // right now the useState gets the string from the user input
+
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+
+  // holder of result
+
+  const [result, setResult] = useState("");
+
+  // setNumx mimics
+  // const setNum1 = function () {}
+
+  // handle the change state of num1
+  const handleNum1Change = (event) => {
+    setNum1(event.target.value);
+    console.log(num1);
+  };
+
+  const handleNum2Change = (event) => {
+    setNum2(event.target.value);
+    console.log(num1);
+  };
+
+  const handleAddition = () => {
+    let add = Number(num1) + Number(num2);
+    setResult(add);
+  }
+
+  const handleSubtraction = () => {
+    let minus = Number(num1) - Number(num2);
+    setResult(minus);
+  }
+
+  const handleMultiplication = () => {
+    let multiply = Number(num1) * Number(num2);
+    setResult(multiply);
+  }
+
+  const handleDivision = () => {
+    let divide = Number(num1) / Number(num2);
+    setResult(divide);
+  }
+
 
   return (
     <body>
@@ -49,6 +99,19 @@ export default function App() {
             );
           })} */}
         </div>
+      </div>
+      <div className="calculator">
+        <h1>Simple Calculator</h1>
+        <label>Num 1:</label>
+        <input type="number" onChange={handleNum1Change}></input>
+        <label>Num 2:</label>
+        <input type="number" onChange={handleNum2Change}></input>
+        <br />
+        <button onClick={handleAddition}>Add</button>
+        <button onClick={handleSubtraction}>Subtract</button>
+        <button onClick={handleMultiplication}>Multiply</button>
+        <button onClick={handleDivision}>Divide</button>
+        <h1>Result: {result}</h1>
       </div>
     </body>
 
